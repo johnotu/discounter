b70818f87957061905c76defea384ecfb73d4dda
John Otu <johnne01@gmail.com>
2019-08-08 23:53:45 +0100

    added error boundary

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Group from './components/Group';
 import Signup from './components/Signup';
 import Analytics from './components/Analytics';
 import NewGroup from './components/NewGroup';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App = () => {
   return (
+    <ErrorBoundary>
     <Router>
       <Route path='/' exact component={Groups} />
       <Route path='/group' exact component={Groups} />
@@ -16,7 +18,8 @@ const App = () => {
       <Route path='/signup/:id' exact component={Signup} />
       <Route path='/analytics' exact component={Analytics} />
       <Route path='/new-group' exact component={NewGroup} />
-    </Router>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
