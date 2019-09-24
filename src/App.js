4b1815a0ee22288c9023076dbefbda44c6ae1f18
John Otu <johnne01@gmail.com>
2019-09-24 13:37:28 +0100

    Change group signup url
    So it does not clash with account signup

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Signup from './components/Signup';
 import Analytics from './components/Analytics';
 import NewGroup from './components/NewGroup';
 import ErrorBoundary from './components/ErrorBoundary';
+// import AccountSignup from './components/AccountSignup';
 
 const App = () => {
   return (
@@ -22,9 +23,10 @@ const App = () => {
       <Route path='/' exact render={props =>  <Groups {...props} groups={groups} />} />
       <Route path='/group' exact render={props => <Groups {...props} groups={groups} />} />
         <Route path='/group/:id' exact render={props => <Group {...props} group={groups.find(group => group.id === props.match.params.id)} />} />
-        <Route path='/signup/:id' exact render={props => <Signup {...props} group={groups.find(group => group.id === props.match.params.id)} />} />
+        <Route path='/group-signup/:id' exact render={props => <Signup {...props} group={groups.find(group => group.id === props.match.params.id)} />} />
       <Route path='/analytics' exact render={props => <Analytics {...props} groups={groups} />} />
-      <Route path='/new-group' exact component={NewGroup} />
+        <Route path='/new-group' exact component={NewGroup} />
+        {/* <Route path='/signup' exact render component={AccountSignup} /> */}
       </Router>
     </ErrorBoundary>
   );
