5cc3d4daa1750c2a5fb9da5defd2e80ae67942a1
John Otu <johnne01@gmail.com>
2019-09-26 23:58:43 +0100

    Feather icons
    Added feather icons to top and side navs

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,18 +3,20 @@
  */
 
 import React from 'react';
+import { Users, BarChart } from 'react-feather';
 
 const SideNav = props => {
   return (
-    <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+    <nav className="col-md-2 d-none d-md-block sidebar">
       <div className="sidebar-sticky">
         <ul className="nav flex-column">
           <li className="nav-item">
-            <a className={`nav-link ${props.groupsActive ? 'active' : ''}`} href="/">Groups</a>
+            <a className={`nav-link ${props.groupsActive ? 'active' : ''}`} href="/groups">
+              <Users size={14} style={{ marginRight: '10', verticalAlign: '-2px' }} /> Groups</a>
           </li>
           <li className="nav-item">
             <a className={`nav-link ${props.analyticsActive ? 'active' : ''}`} href="/analytics">
-              Analytics
+              <BarChart size={14} style={{ marginRight: '10', verticalAlign: '-2px'}} />Analytics
             </a>
           </li>
         </ul>
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -3,11 +3,22 @@
  */
 
 import React from 'react';
+import { User } from 'react-feather';
 
 const TopNav = props => {
   return (
-    <nav className="navbar navbar-light fixed-top bg-info flex-md-nowrap p-0">
-      <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/"><h5 className="text-white">Discounter</h5></a>
+    <nav className="navbar navbar-light bg-light fixed-top flex-md-nowrap p-0">
+      <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/">
+        <h5 className="text-info">Discounter</h5>
+      </a>
+      <ul className="navbar-nav px-3">
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            <User className="text-info" />
+          </a>
+        </li>
+      </ul>
+      
     </nav>
   );
 }
