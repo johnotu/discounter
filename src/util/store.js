9d9d1d4a4d22714c77f3162ea934522e9a397fb6
John Otu <johnne01@gmail.com>
2019-10-07 22:08:03 +0100

    Store

--- src/util/store.js
+++ src/util/store.js
@@ -1,6 +1,6 @@
 const setData = (key, value) => localStorage.setItem(key, JSON.stringify(value));
 
-const getData = key => typeof localStorage.getItem(key) === undefined || typeof localStorage.getItem(key) === null ? null :  JSON.parse(localStorage.getItem(key));
+const getData = key => localStorage.getItem(key) || null;
 
 const clearData = key => localStorage.clear(key);
 
