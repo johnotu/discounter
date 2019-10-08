2fcaea1fd59e2217e8675f0f3441a85c34f029ea
John Otu <johnne01@gmail.com>
2019-10-08 09:37:38 +0100

    Landing page
    Populated content

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,35 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import TopNav from './TopNav';
+import Ill from '../images/process.svg';
 
 const Home = props => {
 
   return (
-    <div className="admin">
+    <div className="admin bg-light" style={{height: '100vh'}}>
       <TopNav home />
       <div className="container-fluid">
         <div className="row">
-          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-            
+          <main role="main">
+            <div className="home-content">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="landing-text">
+                    <h1 className="text-align-left heading">Group discount manager for marketers</h1>
+                    <p className="lead">
+                      {/* Do you offer group discounts for your goods or services? */}
+                      Discounter makes it easy to set up and manage group discounts from start to finish.</p>
+                    <a href="/signup" className="btn btn-info btn-lg">Create a free account</a>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="landing-image">
+                    <img src={Ill} alt="Illustration" width="80%" />
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </main>
         </div>
       </div>
